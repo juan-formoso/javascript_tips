38d2f9cc963ee0a95ec688bf9244fee688067f72
Juan Formoso <trajano.juan@gmail.com>
2021-11-24 09:54:51 -0400

    Add variable assignments tips

---
+++ variable-assignment.js
@@ -0,0 +1,22 @@
+// AUTO ASSIGNMENT OF VARIABLES IN JAVASCRIPT
+// Instead of assign values to numeric variables on this way:
+
+x = x + 1;
+minus = minus - 1;
+y = y * 10;
+
+// You can use 'clean' versions like this:
+
+x++;
+minus--;
+y*=10;
+
+// MORE...
+// If you didn't understand the third example (*=) consider that 'x = 10' and 'y = 5' to the following expressions;
+// Showing it in a pratical way:
+
+x += y // results x=15
+x -= y // results x=5
+x *= y // results x=50
+x /= y // results x=2
+x %= y // results x=0
