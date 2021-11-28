35e6fa6af36d890e9cc25847da5b5efa129cf59e
Juan Formoso <trajano.juan@gmail.com>
2021-11-28 11:21:24 -0400

    Add promise concept

---
+++ promise.js
@@ -0,0 +1,10 @@
+// WHAT IS A 'PROMISE' IN JAVASCRIPT?
+
+// In simple words, a promise is an object that may produce and store a future result for an asynchronous operation.
+// A promise may produce either a resolved value, or a reason why it's not resolved (e.g., a network error ocurred)
+
+// A Promise is in one of these states:
+// pending: initial state, neither fullfiled nor rejected.
+// fulfilled: meaning that the operation was completed successfully.
+// rejected: meaning that the operation failed.
+// A pending promise can either be fulfilled with a value or rejected with a reason (error).
