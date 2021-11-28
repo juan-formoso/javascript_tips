11b606f320b55577a94f5d53a94c690cde8d32de
Juan Formoso <trajano.juan@gmail.com>
2021-11-28 11:30:31 -0400

    Add Promise tip

--- promise.js
+++ promise.js
@@ -8,3 +8,44 @@
 // fulfilled: meaning that the operation was completed successfully.
 // rejected: meaning that the operation failed.
 // A pending promise can either be fulfilled with a value or rejected with a reason (error).
+
+// When either of the 3 options happens, the associated handlers queued up by a promise's then method are called.
+// Every promise must supply a.then() method with the following signature:
+
+
+promise.then(
+	function(result) { /* handle a sucessful result */ }, 
+	function(error) { /* handle an error */ }
+);
+
+
+// For instance, here's a reaction to a successfully resolved promise:
+
+
+//Creating a new promise
+let promise = new Promise(function(resolve, reject){
+	//Manually resolving the promise for understanding
+	setTimeout(() => resolve("done!"), 1000);
+});
+
+// resolve runs the first function in .then
+promise.then(
+	result => alert(result), // shows "done!" after 1000
+	error => alert(error) // doesn't run
+);
+
+
+// Here's a reaction to a succesfully resolved promise:
+
+
+//Creating a new promise
+let promise = new Promise(function(resolve, reject){
+	//Manually resolving the promise for understanding
+	setTimeout(() => reject(new Error("Whooops!")), 1000);
+});
+
+// reject runs the first function in .then
+promise.then(
+	result => alert(result), // doesn't run
+	error => alert(error) // shows "Error: Whooops!" after 1000
+);
